11fe828d386310922d4540461138958904edc573
Annisingh003 <anikettheking2004@gmail.com>
2026-04-05 18:12:44 +0530

    Fix resume filename and link

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -70,7 +70,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="/latest _resume.pdf" target="_blank">
+      <a className="resume-button" href="/latest_resume.pdf" target="_blank">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
